e8b755932194a67430c0e4bec6ffef34349f8d03
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-06-07 21:07:07 +0900

    Add LocalTask data class

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/local/LocalTask.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/local/LocalTask.kt
@@ -15,3 +15,14 @@
  */
 
 package com.example.android.architecture.blueprints.todoapp.data.source.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task")
+data class LocalTask(
+    @PrimaryKey val id: String,
+    val title: String,
+    val description: String,
+    val isCompleted: Boolean
+)
